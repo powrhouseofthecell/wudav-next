0ccea90c08949bfed17a2537cf88af2c510502e0
Zuhaib <theshahzuhaib96@gmail.com>
2022-09-05 18:01:12 +0530

    Add: Nav link

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,11 +1,11 @@
 const headerNavLinks = [
    { href: '/work', title: 'Work' },
-   { href: '/about', title: 'About' },
    { href: '/expertise', title: 'Expertise' },
    { href: '/blog', title: 'Blog' },
    { href: '/tags', title: 'Tags' },
    { href: '/careers', title: 'Careers' },
    { href: '/contact', title: 'Contact' },
+   { href: '/about', title: 'About' },
 ];
 
 export default headerNavLinks;
