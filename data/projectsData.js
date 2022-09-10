7ed20e2f43ea8fc6b318cbd38b8369c7fb68bcb1
Zuhaib <theshahzuhaib96@gmail.com>
2022-09-10 11:25:10 +0530

    Add: Images for work-routes

--- data/projectsData.js
+++ data/projectsData.js
@@ -6,114 +6,152 @@ const projectsData = [
       `,
       imgSrc: '/static/images/work/fibr.jpg',
       href: '/work/fibr',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Efruitmandi',
       description: `The client wanted a brand identity which should reflect Kashmir’s major fruit produce sought after globally.`,
       imgSrc: '/static/images/work/efruit.jpg',
       href: '/work/efruitmandi',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Illustrious Consulting',
       description: `The client brief wanted a corporate identity that should reflect seriousness about the finances and should also represent accounting as the strength. `,
       imgSrc: '/static/images/work/illustrious.jpg',
       href: '/work/illustrious-consulting',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Investgate',
       description: `The client approached us for complete branding and social media campaigns including the nomenclature. `,
       imgSrc: '/static/images/work/investgate.jpg',
       href: '/work/investgate',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'First Digital Bank',
       description: `The client brief asked for a visual identity that should communicate flow of liquidity with four different colors representing four building principles of the bank. `,
       imgSrc: '/static/images/work/fdb.jpg',
       href: '/work/first-digital-bank',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Construction Souq',
       description: `The client approached us for the content development for the website and other collaterals.`,
       imgSrc: '/static/images/work/constructionsouq.jpg',
       href: '/work/construction-souq',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Bhalanetra',
       description: `The client approached us with a brief asking for creating the corporate identity which can help them connect with its target audience. `,
       imgSrc: '/static/images/work/bhalanetra.jpg',
       href: '/work/bhalanetra',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Taameir',
       description: `We developed the brand identity and some of the product banners for the portal.`,
       imgSrc: '/static/images/work/taameir.jpg',
       href: '/work/taameir',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Shadaab',
       description: ``,
       imgSrc: '/static/images/work/shadaab.jpg',
       href: '/work/shadaab',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Linkoll',
       description: `We developed the corporate identity and the launch strategy and content for the brand. A clean design identity with ‘L’ and overlapping ‘O’ was created.`,
       imgSrc: '/static/images/work/linkoll.jpg',
       href: '/work/linkoll',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Flykash',
       description: `We helped the brand to create its logo for its website and app. The logo reflects the essence of Kashmir and travel with ‘K’ of Kash with the help of the tail wings of a plane.`,
       imgSrc: '/static/images/work/flykash.jpg',
       href: '/work/flykash',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Aquatic',
       description: `We got the assignment to design the brand identity and the packaging for the product.`,
       imgSrc: '/static/images/work/aquatic.jpg',
       href: '/work/aquatic',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'My Rahat',
       description: `The brand identity of myRahat was redesigned by us keeping in mind the social impact of the enterprise. We also helped myRahat with end to end communication strategy for the brand.`,
       imgSrc: '/static/images/work/myrahat.jpg',
       href: '/work/my-rahat',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Swift Homes',
       description: `We have been handling the social media content and design services for Swift Homes. All content and design is created keeping the Kashmir’s audience in mind. `,
       imgSrc: '/static/images/work/swifthomes.jpg',
       href: '/work/swift-homes',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Punn',
       description: `Wudav Brand Stories launched the brand on social media by creating a launch video depicting the genesis of the authentic Kashmiri Shawls.`,
       imgSrc: '/static/images/work/punn.jpg',
       href: '/work/punn',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Smart Edg',
       description: `Smartedg is a digital service which has incorporated state-of-the-art technological and managerial tools and techniques that aid in improving efficiency, saving time and money.`,
       imgSrc: '/static/images/work/smartedg.jpg',
       href: '/work/edg',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Aarafh',
       description: `Founded in state-of-the-art manufacturing facilities, innovative techniques, ever-improving research and development, rigorous quality checks.`,
       imgSrc: '/static/images/work/aarafh.jpg',
       href: '/work/aarafh',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Savoll',
       description: `Savoll empowers merchants to grow their business and enables customers to save money.`,
       imgSrc: '/static/images/work/savoll.jpg',
       href: '/work/savoll',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
    {
       title: 'Enmagic',
       description: `The client wanted a complete brand identity development for their business. `,
       imgSrc: '/static/images/work/enmagic.jpg',
       href: '/work/Enmagic',
+      fullDetails:
+         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa adipisci voluptatemharum natus asperiores est atque aperiam architecto accusamus numquam veniam consecteturexpedita, cum, sed magni maiores. Earum, sit corporis tenetur doloribus ratione, ex tempore impedit vel quasi qui dignissimos saepe explicabo est iste iure vitae rem placeat ab voluptate tempora. Repudiandae magni rerum, omnis totam eius possimus, sapiente, magnam inventore explicabo facere iusto voluptatem? Iure alias odio voluptas architecto, accusamus at eos officia voluptatibus quia modi quibusdam. Autem suscipit aliquid illo adipisci error, esse rem non excepturi explicabo architecto nostrum dignissimos quam est mollitia commodi! Voluptatum, voluptate vitae!',
    },
 ];
 
--- pages/work/[client].js
+++ pages/work/[client].js
@@ -1,11 +1,20 @@
 import { useRouter } from 'next/router';
-import styles from '../../css/Home.module.css';
+import Image from 'next/image';
+
+import projectsData from '@/data/projectsData';
+
 const Client = () => {
    const router = useRouter();
    const clientName = router.query.client;
+
+   const imgSource = `/static/images/work/${clientName}.jpg`;
+   // console.log(projectsData.fullDetails);
+
    return (
       <>
-         <h1 className={styles.center}>Client Page {clientName}</h1>
+         <h1 className="">{clientName}</h1>
+         <Image alt="Client Image" className="" width={1200} height={500} src={imgSource} />
+         <p>{projectsData.fullDetails}</p>
       </>
    );
 };
