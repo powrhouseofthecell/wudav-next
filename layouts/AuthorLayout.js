a2ededab6303688a0329221196e245a1c9302b84
Zuhaib <theshahzuhaib96@gmail.com>
2022-09-18 14:32:02 +0530

    REVERT

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -2,8 +2,6 @@ import SocialIcon from '@/components/social-icons';
 import Image from '@/components/Image';
 import { PageSEO } from '@/components/SEO';
 
-import styles from '../css/general.module.css';
-
 export default function AuthorLayout({ children, frontMatter }) {
    const { name, avatar, occupation, company, email, twitter, linkedin, github } = frontMatter;
 
@@ -12,9 +10,7 @@ export default function AuthorLayout({ children, frontMatter }) {
          <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
          <div className="divide-y divide-gray-200 dark:divide-gray-700">
             <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-               <h1
-                  className={`${styles.animateGradientText} text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14`}
-               >
+               <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
                   About
                </h1>
             </div>
--- pages/blog.js
+++ pages/blog.js
@@ -24,7 +24,7 @@ export default function Blog({ posts, initialDisplayPosts, pagination }) {
             posts={posts}
             initialDisplayPosts={initialDisplayPosts}
             pagination={pagination}
-            title="Blogs"
+            title="All Blogs"
          />
       </>
    );
