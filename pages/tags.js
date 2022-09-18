e9e9a83b2c3e48d29ec617b8420f2e01d437b7b9
Zuhaib <theshahzuhaib96@gmail.com>
2022-09-18 13:03:14 +0530

    Add: Heading gradient animation

--- pages/tags.js
+++ pages/tags.js
@@ -5,6 +5,8 @@ import siteMetadata from '@/data/siteMetadata';
 import { getAllTags } from '@/lib/tags';
 import kebabCase from '@/lib/utils/kebabCase';
 
+import styles from '../css/general.module.css';
+
 export async function getStaticProps() {
    const tags = await getAllTags('blog');
 
@@ -21,7 +23,9 @@ export default function Tags({ tags }) {
          />
          <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
             <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-               <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
+               <h1
+                  className={`${styles.animateGradientText} h-20 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14`}
+               >
                   Tags
                </h1>
             </div>
