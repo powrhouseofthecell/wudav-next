73103b885dfeece3e65006349e9a84fae60335b8
Zuhaib <theshahzuhaib96@gmail.com>
2022-09-19 14:42:48 +0530

    Tag to end

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,10 +2,10 @@ const headerNavLinks = [
    { href: '/work', title: 'Work' },
    { href: '/expertise', title: 'Expertise' },
    { href: '/blog', title: 'Blogs' },
-   { href: '/tags', title: 'Tags' },
    { href: '/careers', title: 'Careers' },
    { href: '/contact', title: 'Contact' },
    { href: '/about', title: 'About' },
+   { href: '/tags', title: 'Tags' },
 ];
 
 export default headerNavLinks;
