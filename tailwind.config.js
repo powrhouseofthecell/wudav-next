fec7a7133bfc26e06ec94bd23319be6287f95f03
Zuhaib <theshahzuhaib96@gmail.com>
2022-10-06 12:23:11 +0530

    :wrench: Delete Comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
-//
 module.exports = {
    experimental: {
       optimizeUniversalDefaults: true,
