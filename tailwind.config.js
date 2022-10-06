4f79690d4848c7ba1dc33425a5631e074faf8b5e
Zuhaib <theshahzuhaib96@gmail.com>
2022-10-06 12:22:49 +0530

    :wrench: To Add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
+//
 module.exports = {
    experimental: {
       optimizeUniversalDefaults: true,
