938f701dd1147bcbfa53a93cf6823ed5a3d735cc
Zuhaib <theshahzuhaib96@gmail.com>
2022-10-14 12:46:00 +0530

    🧑‍💻 About to the end

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -4,8 +4,8 @@ const headerNavLinks = [
    { href: '/blog', title: 'Blogs' },
    { href: '/careers', title: 'Careers' },
    { href: '/contact', title: 'Contact' },
-   { href: '/about', title: 'About' },
    { href: '/tags', title: 'Tags' },
+   { href: '/about', title: 'About' },
 ];
 
 export default headerNavLinks;
