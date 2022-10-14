51d423287a0eaeaadf60b5270dd94a1c49aad059
Zuhaib <theshahzuhaib96@gmail.com>
2022-10-14 12:46:53 +0530

    💡 Add space

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -6,6 +6,7 @@ import Image from '@/components/Image';
 import Tag from '@/components/Tag';
 import siteMetadata from '@/data/siteMetadata';
 import ScrollTopAndComment from '@/components/ScrollTopAndComment';
+
 // import Comments from '@/components/comments';
 
 // const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`;
