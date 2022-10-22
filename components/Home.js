a4871aefb92b39a3996fd4975b2feae862cad64e
Zuhaib <theshahzuhaib96@gmail.com>
2022-10-22 16:49:44 +0530

    ✨ Add text animations in Hompage

--- components/Home.js
+++ components/Home.js
@@ -3,6 +3,7 @@ import siteMetadata from '@/data/siteMetadata';
 import Image from 'next/image';
 
 import styles from '../css/general.module.css';
+import textStyles from '../css/text.module.css';
 
 const clientLogos = ['1', '2', '3', '4']; // Image Names for the client logos
 
@@ -11,20 +12,55 @@ const Home = () => {
       <>
          <PageSEO title={`Home - ${siteMetadata.title}`} description={siteMetadata.description} />
          <section className="home">
-            {/* Home Secion */}
+            {/* Hero Secion */}
             <div className={`${styles.hero}`}>
-               <h1 className="text-center text-6xl">Hello Wudav</h1>
+               <div className={`${textStyles.content}`}>
+                  <div className={`${textStyles.content__container}`}>
+                     <ul className={`${textStyles.content__container__list}`}>
+                        <li className={`${textStyles.content__container__list__item}`}>
+                           Communicate
+                        </li>
+                        <li className={`${textStyles.content__container__list__item}`}>Connect</li>
+                        <li className={`${textStyles.content__container__list__item}`}>Showcase</li>
+                        <li className={`${textStyles.content__container__list__item}`}>
+                           Reach People
+                        </li>
+                        <li className={`${textStyles.content__container__list__item}`}>
+                           Differentiate
+                        </li>
+                        <li className={`${textStyles.content__container__list__item}`}>
+                           Build Brands
+                        </li>
+                        <li className={`${textStyles.content__container__list__item}`}>
+                           Visual Stories
+                        </li>
+                     </ul>
+                  </div>
+               </div>
             </div>
-            {/* Home Secion End */}
+            {/* Hero Secion End */}
 
             {/* Quote Secion */}
             <div className="quote h-full text-center">
-               <h2 className="text-4xl">Wudav Brand Stories</h2>
+               <h2 className={`${styles.animateGradientText} my-8 text-5xl font-bold`}>
+                  Wudav Brand Stories
+               </h2>
+               <div className={`${styles.panel}`}>
+                  <h1 className={`${styles.jj}`}>
+                     <span>Wudav Brand Stories</span>
+                     {/* <span></span> */}
+                  </h1>
+                  <h1 className={`${styles.jj}`}>
+                     <span>Wudav Brand Stories</span>
+                     {/* <span></span> */}
+                  </h1>
+               </div>
                <figure>
-                  <q>Creativity is intelligence having fun!</q>
-                  <figcaption>ALBERT EINSTEIN</figcaption>
+                  <q className="text-2xl">Creativity is intelligence having fun!</q>
+                  <figcaption className="text-ml">ALBERT EINSTEIN</figcaption>
+                  <hr className="my-8" />
                </figure>
-               <p>
+               <p className="my-8">
                   Embracing is a skill. That is what we help your audience learn - to embrace your
                   brand. With a word or without one, imbibing it into the minds of your audience is
                   very important. Whether you are already an established brand or have just started
@@ -34,12 +70,15 @@ const Home = () => {
                   At Wudav Brand Stories , we help brands communicate and grow with aplomb. We do no
                   witchcraft; rather we work very hard to cut through the clutter.
                </p>
+               <hr className="my-8" />
             </div>
             {/* Quote Secion */}
 
             {/* Client Section */}
             <section>
-               <h1 className="text-center text-4xl">Our Clients</h1>
+               <h1 className={`${styles.animateGradientText} my-10 text-center text-5xl font-bold`}>
+                  Our Clients
+               </h1>
                <div className={`${styles.mainContainer}`}>
                   <ul className={`${styles.gridWrapper}`}>
                      <li>
