4e61e01f1ea299109e52ff6b75c9e6da20a1b889
Zuhaib <theshahzuhaib96@gmail.com>
2022-10-25 19:47:32 +0530

    ✨ See more button

--- components/ViewMoreButton.js
+++ components/ViewMoreButton.js
@@ -1,7 +1,21 @@
 import styles from '../css/viewmorebutton.module.css';
 
 const ViewMoreButton = () => {
-   return <button className={`${styles.btn} ${styles.draw_border}`}>Draw Border</button>;
+   return (
+      <section className={`${styles.buttons} ${styles.section}`}>
+         <div className={`${styles.container}`}>
+            {/* <a href="https://twitter.com/Dave_Conner" className={`${styles.btn} ${styles.btn_1}`}>
+               <svg>
+                  <rect x="0" y="0" fill="none" width="100%" height="100%" />
+               </svg>
+               See More
+            </a> */}
+            <a href="" className={`${styles.btn} ${styles.btn_2}`}>
+               See More
+            </a>
+         </div>
+      </section>
+   );
 };
 
 export default ViewMoreButton;
