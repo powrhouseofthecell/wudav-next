03261c8f48acecd872b107fa163ce860e83e729b
Zuhaib <theshahzuhaib96@gmail.com>
2022-10-28 15:41:25 +0530

    ⚡️ Fix z-index

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -39,7 +39,8 @@ const MobileNav = () => {
             </svg>
          </button>
          <div
-            className={`fixed top-0 left-0 z-10 h-full w-full transform bg-gray-200 opacity-95 duration-300 ease-in-out dark:bg-gray-800 ${
+            // Look out for the z-index
+            className={`fixed top-0 left-0 z-40 h-full w-full transform bg-gray-200 opacity-95 duration-300 ease-in-out dark:bg-gray-800 ${
                navShow ? 'translate-x-0' : 'translate-x-full'
             }`}
          >
