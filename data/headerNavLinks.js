12bf73f06cb98ce882c040b4bc222c592b754a6c
Zuhaib <theshahzuhaib96@gmail.com>
2022-10-28 16:09:22 +0530

    🚧 Add links for test.

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,10 +1,10 @@
 const headerNavLinks = [
    { href: '/work', title: 'Work' },
-   // { href: '/expertise', title: 'Expertise' },
+   { href: '/expertise', title: 'Expertise' },
    { href: '/blog', title: 'Blogs' },
    { href: '/careers', title: 'Careers' },
    { href: '/contact', title: 'Contact' },
-   // { href: '/tags', title: 'Tags' },
+   { href: '/studies', title: 'Case Studies' },
    { href: '/about', title: 'About' },
 ];
 
